4e9fafa74b12d3e672418d2af198c7c52cfcb966
Terence Kruger <terencejkruger@gmail.com>
2021-01-14 23:32:37 +1030

    Ranaming

--- src/AggregateRoot.js
+++ src/AggregateRoot.js
@@ -1,15 +1,12 @@
 import { each } from 'lodash';
 
-class Aggregate {
-  lastUpdated = new Date();
-
+class AggregateRoot {
   apply = function (events) {
     each(events, event => {
       if (this[event.eventType]) {
         this[event.eventType](event);
-        this.lastUpdated = new Date();
       }
     });
   };
 }
-export default Aggregate;
+export default AggregateRoot;
